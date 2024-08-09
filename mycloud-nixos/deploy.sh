9c7d89182deb765f0e25c2677113cdf2e1a44d26
Romeo Valentin <romeov@stanford.edu>
2024-08-09 15:27:33 +0200

    deploy: pass all inputs via ~-I~
    This should replace having to set up all the channels.

--- mycloud-nixos/deploy.sh
+++ mycloud-nixos/deploy.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 
-nixos-rebuild switch --target-host hetzner -I nixos-config=./configuration.nix -I agenix=https://github.com/ryantm/agenix/archive/main.tar.gz "$@"
+nixos-rebuild switch --target-host hetzner \
+    -I nixos-config=./configuration.nix \
+    -I nixos-unstable=https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz \
+    -I agenix=https://github.com/ryantm/agenix/archive/main.tar.gz \
+    "$@"
