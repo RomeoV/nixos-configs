f3d4f2ef757031336f540cb3467f0e698b205214
Romeo Valentin <romeov@stanford.edu>
2024-09-09 13:25:52 +0200

    syncthing: Add key and cert to age

--- mycloud-nixos-2/agenix/secrets.nix
+++ mycloud-nixos-2/agenix/secrets.nix
@@ -14,4 +14,6 @@ in {
   "backblaze_repo_2.age".publicKeys = [ Lenovo-P1 JuiceSSH mycloud-nixos-2];
   "mlflow-artifacts-key.age".publicKeys = [ Lenovo-P1 JuiceSSH mycloud-nixos mycloud-nixos-2 ];
   "paperless-admin-password.age".publicKeys = [ Lenovo-P1 mycloud-nixos mycloud-nixos-2 ];
+  "syncthing-key.age".publicKeys = [ Lenovo-P1 mycloud-nixos-2 ];
+  "syncthing-cert.age".publicKeys = [ Lenovo-P1 mycloud-nixos-2 ];
 }
