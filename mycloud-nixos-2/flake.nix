29656f3b5c1eb542276a1cbfa45d43ff19cd837f
Romeo Valentin <romeov@stanford.edu>
2024-11-29 22:17:19 -0800

    Bump to nixos-24.11

--- mycloud-nixos-2/flake.nix
+++ mycloud-nixos-2/flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs-master.url = "github:NixOS/nixpkgs/3259cf03626f8fd2f54c67becd531b9276885a64";
     agenix.url = "github:ryantm/agenix";
