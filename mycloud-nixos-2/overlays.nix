748311a61591a3db70836851b6305fb45da029d6
Romeo Valentin <romeov@stanford.edu>
2024-11-30 00:07:57 -0800

    Delete old mlflow attempts (overlays, patches, ...)

--- mycloud-nixos-2/overlays.nix
+++
@@ -1,9 +0,0 @@
-{ }:
-
-{
-  nixpkgs.config.packageOverrides = pkgs: {
-    mlflow-server = pkgs.mlflow-server.overrideAttrs (oldAttrs: {
-      patches = (oldAttrs.patches or []) ++ [ ./patches/mlflow-server.nix.patch ];
-    });
-  };
-}
