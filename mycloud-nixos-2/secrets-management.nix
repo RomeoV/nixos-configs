7e95129c023dc355c11ac1b9f3a02e7510a87e18
Romeo Valentin <romeov@stanford.edu>
2024-12-27 17:42:40 +0100

    secrets: Move all secrets to extra file.

--- mycloud-nixos-2/secrets-management.nix
+++ mycloud-nixos-2/secrets-management.nix
@@ -1,6 +1,4 @@
-{ ... }:
-
-{
+{ ... }: {
   age.secrets = {
     nextcloud_admin_pass = {
       file = agenix/nextcloud_admin_pass.age;
@@ -10,8 +8,15 @@
       file = agenix/hetzner_private_key.age;
       owner = "root";
     };
-    backblaze_env.file = agenix/backblaze_env.age;
-    backblaze_repo.file = agenix/backblaze_repo.age;
-    backblaze_password.file = agenix/backblaze_password.age;
+    backblaze_env_2.file = agenix/backblaze_env_2.age;
+    backblaze_repo_2.file = agenix/backblaze_repo_2.age;
+    backblaze_password_2.file = agenix/backblaze_password_2.age;
+    mlflow-artifacts-key.file = agenix/mlflow-artifacts-key.age;
+    paperless-admin-password.file = agenix/paperless-admin-password.age;
+    syncthing-key.file = agenix/syncthing-key.age;
+    syncthing-cert.file = agenix/syncthing-cert.age;
+    porkbun-secret-api-key-both.file = agenix/porkbun-secret-api-key-both.age;
+    github-key.file = agenix/github-key.age;
+    rclone-config-immich-object-storage.file = agenix/rclone-config-immich-object-storage.age;
   };
 }
--- mycloud-nixos-2/system-configuration.nix
+++ mycloud-nixos-2/system-configuration.nix
@@ -102,27 +102,6 @@
   ];
 
 
-  age.secrets = {
-    nextcloud_admin_pass = {
-      file = agenix/nextcloud_admin_pass.age;
-      owner = "nextcloud";
-    };
-    hetzner_private_key = {
-      file = agenix/hetzner_private_key.age;
-      owner = "root";
-    };
-    backblaze_env_2.file = agenix/backblaze_env_2.age;
-    backblaze_repo_2.file = agenix/backblaze_repo_2.age;
-    backblaze_password_2.file = agenix/backblaze_password_2.age;
-    mlflow-artifacts-key.file = agenix/mlflow-artifacts-key.age;
-    paperless-admin-password.file = agenix/paperless-admin-password.age;
-    syncthing-key.file = agenix/syncthing-key.age;
-    syncthing-cert.file = agenix/syncthing-cert.age;
-    porkbun-secret-api-key-both.file = agenix/porkbun-secret-api-key-both.age;
-    github-key.file = agenix/github-key.age;
-    rclone-config-immich-object-storage.file = agenix/rclone-config-immich-object-storage.age;
-  };
-
 
 
   services.netdata.enable = true;
