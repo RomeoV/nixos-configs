99b125a9ac02d572d309e8c242dc66351c08a298
Romeo Valentin <romeov@stanford.edu>
2024-12-30 21:46:33 +0100

    Add tailscale nameserver.

--- mycloud-nixos/networking.nix
+++ mycloud-nixos/networking.nix
@@ -2,10 +2,12 @@
   # This file was populated at runtime with the networking
   # details gathered from the active system.
   networking = {
-    nameservers = [ "2a01:4ff:ff00::add:2"
- "2a01:4ff:ff00::add:1"
- "185.12.64.1"
- ];
+    nameservers = [
+      "100.64.0.254"
+      "2a01:4ff:ff00::add:2"
+      "2a01:4ff:ff00::add:1"
+      "185.12.64.1"
+    ];
     defaultGateway = "172.31.1.1";
     defaultGateway6 = {
       address = "fe80::1";
@@ -20,7 +22,7 @@
         ];
         ipv6.addresses = [
           { address="2a01:4ff:f0:e2df::1"; prefixLength=64; }
-{ address="fe80::9400:1ff:fe96:46f0"; prefixLength=64; }
+          { address="fe80::9400:1ff:fe96:46f0"; prefixLength=64; }
         ];
         ipv4.routes = [ { address = "172.31.1.1"; prefixLength = 32; } ];
         ipv6.routes = [ { address = "fe80::1"; prefixLength = 128; } ];
