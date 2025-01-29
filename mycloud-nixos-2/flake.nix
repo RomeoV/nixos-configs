ee75a5ca74bf49a4cf2b68bfc705923da82d113e
Romeo Valentin <romeov@stanford.edu>
2025-01-29 14:20:55 -0800

    nit: keep redlib inputs updated.

--- mycloud-nixos-2/flake.nix
+++ mycloud-nixos-2/flake.nix
@@ -5,9 +5,14 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs-mlflow.url = "github:NixOS/nixpkgs/3259cf03626f8fd2f54c67becd531b9276885a64";
-    agenix.url = "github:ryantm/agenix";
-    agenix.inputs.darwin.follows = "";
-    redlib.url = "github:RomeoV/redlib";
+    agenix ={
+      url = "github:ryantm/agenix";
+      inputs.darwin.follows = "";
+    };
+    redlib = {
+      url = "github:RomeoV/redlib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     sbucaptions-webserver.url = "git+ssh://git@github/RomeoV/sbucaptions-webserver?rev=ea12ee93a37abd5cdece9e15e9eba0b9fe63e3ff";
     # sbucaptions-webserver.inputs.nixpkgs.follows = "nixpkgs";
   };
