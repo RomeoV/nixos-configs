5f04dd91f665bb7e75a95d882228278e18499307
Romeo Valentin <romeov@stanford.edu>
2025-03-05 23:58:50 -0800

    Add comment to deploy script.

--- mycloud-nixos-2/deploy.sh
+++ mycloud-nixos-2/deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
+# nix flake update agenda-exporter --override-input agenda-exporter ./agenda-exporter
 nixos-rebuild switch --flake .#mycloud-nixos-2 --target-host hetzner2 "$@"
