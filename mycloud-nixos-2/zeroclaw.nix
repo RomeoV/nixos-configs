64c1b34d6f10f78b963a5dce944f8aefe13aa567
Romeo Valentin <romeov@stanford.edu>
2026-02-20 01:00:59 -0800

    zeroclaw: Use haiku 4.5 for now

--- mycloud-nixos-2/zeroclaw.nix
+++ mycloud-nixos-2/zeroclaw.nix
@@ -21,7 +21,7 @@ in {
   services.zeroclaw = {
     enable = true;
     apiKeyFile = config.age.secrets.zeroclaw-api-key.path;
-    model = "claude-sonnet-4-6";
+    model = "claude-haiku-4-5";
 
     enableCli = true;
     autonomyLevel = "full";
